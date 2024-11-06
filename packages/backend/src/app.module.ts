55b208d22d586beaba8ee8daec8ef65f5ac7048c
kimminsu <xjfcnfw3@naver.com>
2024-11-06 16:54:20 +0900

    ♻️ refactor: typeorm 설정 파일 분리

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-dotenv.config();
+import { typeormConfig } from '@/configs/typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity.{js,ts}'],
-      logging: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(typeormConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
