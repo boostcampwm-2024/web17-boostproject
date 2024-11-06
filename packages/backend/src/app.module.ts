ab25b1b14cd6b9d456acc736e57aad4b7741f1e4
sunghwki <swkim12345@naver.com>
2024-11-06 18:06:00 +0900

    ✨ feat: backend/stock-price 추가

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WinstonModule } from 'nest-winston';
-import { StockPriceModule } from './stock-price/stock-price.module';
-import { OpenapiScraperModule } from './openapi-scraper/openapi-scraper.module';
 import { logger } from '@/configs/logger.config';
 import { typeormConfig } from '@/configs/typeorm.config';
 import { StockModule } from '@/stock/stock.module';
 import { UserModule } from '@/user/user.module';
 import { AuthModule } from '@/auth/auth.module';
+import { OpenapiScraperModule } from '@/openapi-scraper/openapi-scraper.module';
+import { StockPriceModule } from '@/stock-price/stock-price.module';
 
 @Module({
-  imports: [OpenapiScraperModule,
+  imports: [OpenapiScraperModule, StockPriceModule,
     StockModule,
     UserModule,
     TypeOrmModule.forRoot(typeormConfig),
