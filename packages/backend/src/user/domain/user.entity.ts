2f3bd3ac62c3d43afa35f5ec219647eb263ba59a
kimminsu <xjfcnfw3@naver.com>
2024-11-07 10:54:15 +0900

    ✨ feat: 유저 엔티티 구현

---
+++ packages/backend/src/user/domain/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ length: 50 })
+  nickname?: string;
+
+  @Column({ length: 50 })
+  email?: string;
+
+  @Column({ length: 5, default: 'user' })
+  role: string = 'user';
+
+  @Column({ length: 10 })
+  type?: string;
+
+  @Column({ name: 'oauth_id' })
+  oauthId?: number;
+
+  @Column({ name: 'is_light', default: true })
+  isLight: boolean = true;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt?: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt?: Date;
+}
