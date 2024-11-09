719a86b23ef22899e4cf4a2435ded5f69f3f365b
kimminsu <xjfcnfw3@naver.com>
2024-11-09 22:41:14 +0900

    ✨ feat: class validate pipe 설정

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { useSwagger } from '@/configs/swagger.config';
@@ -5,6 +6,7 @@ import { useSwagger } from '@/configs/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   useSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
