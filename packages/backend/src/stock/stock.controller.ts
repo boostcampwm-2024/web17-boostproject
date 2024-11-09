a560408ea42daea1c591c591851fec9ef14da748
kimminsu <xjfcnfw3@naver.com>
2024-11-09 18:07:18 +0900

    ✨ feat: stock controller swagger 추가

--- packages/backend/src/stock/stock.controller.ts
+++ packages/backend/src/stock/stock.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
 import { StockService } from './stock.service';
+import { StockViewRequest } from '@/stock/dto/stockView.request';
 
-export type stockViewRequest = {
-  id: string;
+type MessageResponse = {
+  userStockId?: number;
+  message: string;
 };
 
 @Controller('stock')
@@ -11,7 +14,17 @@ export class StockController {
 
   @HttpCode(200)
   @Post('/view')
-  async increaseStock(@Body() request: stockViewRequest): Promise<void> {
-    await this.stockService.increaseView(request.id);
+  @ApiOperation({
+    summary: '개별 주식 조회수 증가 API',
+    description: '개별 주식 조회수를 증가시킨다.',
+  })
+  @ApiCreatedResponse({
+    description: '유저를 생성한다.',
+  })
+  async increaseStock(
+    @Body() request: StockViewRequest,
+  ): Promise<MessageResponse> {
+    await this.stockService.increaseView(request.stockId);
+    return { message: '주식 조회수가 증가했습니다.' };
   }
 }
