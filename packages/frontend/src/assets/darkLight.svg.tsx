098caa5437be9a000e8f7b1c3703b5a306c4f869
kimminsu <xjfcnfw3@naver.com>
2024-11-10 22:05:09 +0900

    ✨ feat: 네비게이션 바에 필요한 이미지, SVG 추가

---
+++ packages/frontend/src/assets/darkLight.svg.tsx
@@ -0,0 +1,20 @@
+export function DarkLightSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="45px"
+      height="45px"
+      viewBox="0 0 512 512"
+      className={'min-w-[73px]'}
+    >
+      <path
+        fill="gray"
+        d="M414.39 97.61A224 224 0 1 0 97.61 414.39A224 224 0 1 0 414.39 97.61M256 432v-96a80 80 0 0 1 0-160V80c97.05 0 176 79 176 176s-78.95 176-176 176"
+      ></path>
+      <path
+        fill="gray"
+        d="M336 256a80 80 0 0 0-80-80v160a80 80 0 0 0 80-80"
+      ></path>
+    </svg>
+  );
+}
---
+++ packages/frontend/src/assets/user.svg.tsx
@@ -0,0 +1,16 @@
+export function UserSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="45px"
+      height="45px"
+      viewBox="0 0 24 24"
+      className={'min-w-[73px]'}
+    >
+      <g fill="none" stroke="gray" strokeLinecap="round">
+        <circle cx={12} cy={8} r={3.5}></circle>
+        <path d="M4.85 16.948c.639-2.345 3.065-3.448 5.495-3.448h3.31c2.43 0 4.856 1.103 5.496 3.448a10 10 0 0 1 .295 1.553c.06.55-.394.999-.946.999h-13c-.552 0-1.005-.45-.946-.998a10 10 0 0 1 .295-1.554Z"></path>
+      </g>
+    </svg>
+  );
+}
