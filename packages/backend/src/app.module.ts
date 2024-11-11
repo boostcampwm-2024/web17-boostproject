2261dc2a9c772e9c57cbaf1e1d26475f5cb9bc79
kimminsu <xjfcnfw3@naver.com>
2024-11-11 22:10:37 +0900

    ✨ feat: nest session 설정

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,11 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from '@/auth/auth.module';
 import { logger } from '@/configs/logger.config';
 import { typeormConfig } from '@/configs/typeorm.config';
 import { StockModule } from '@/stock/stock.module';
 import { UserModule } from '@/user/user.module';
-import { AuthModule } from '@/auth/auth.module';
 
 @Module({
   imports: [
