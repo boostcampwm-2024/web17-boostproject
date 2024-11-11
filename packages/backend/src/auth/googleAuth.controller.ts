17640c5580a3d5d9dea3815e6e8701dc59ddb60e
kimminsu <xjfcnfw3@naver.com>
2024-11-11 14:51:40 +0900

    ✨ feat: 구글 로그인 페이지 이동 관련 swagger 설정

--- packages/backend/src/auth/googleAuth.controller.ts
+++ packages/backend/src/auth/googleAuth.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { GoogleAuthGuard } from '@/auth/guard/google.guard';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth/google')
 export class GoogleAuthController {
+  @ApiOperation({
+    summary: '구글 로그인 전용 페이지 이동',
+    description: '페이지를 이동하여 구글 로그인을 진행합니다.',
+  })
   @Get('/login')
   @UseGuards(GoogleAuthGuard)
   async handleLogin() {
