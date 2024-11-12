523ed06685f89790abbdb0845abf1d00c38baea9
demian-m00n <m00n.seolmin@gmail.com>
2024-11-12 19:18:34 +0900

    ✨ feat: stock Detail 엔티티 정의

---
+++ packages/backend/src/stock/domain/stockDetail.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Stock } from './stock.entity';
+
+@Entity('stock_detail')
+export class StockDetail {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => Stock)
+  @JoinColumn({ name: 'stock_id' })
+  stock: Stock;
+
+  @Column({
+    type: 'decimal',
+    precision: 20,
+    scale: 2,
+  })
+  marketCap: number;
+
+  @Column({ type: 'integer' })
+  eps: number;
+
+  @Column({ type: 'integer' })
+  per: number;
+
+  @Column({ type: 'integer' })
+  high52w: number;
+
+  @Column({ type: 'integer' })
+  low52w: number;
+}
