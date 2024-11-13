80950f01bfe3dd989b74aed04586119ad7c1922f
demian-m00n <m00n.seolmin@gmail.com>
2024-11-13 23:15:13 +0900

    ✨ feat: get StockData 데코레이터 구현

---
+++ packages/backend/src/stock/decorator/stockData.decorator.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable max-lines-per-function */
+
+import { applyDecorators } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { StockDataResponse } from '../dto/stockData.response';
+
+export function ApiGetStockData(summary: string, type: string) {
+  return applyDecorators(
+    ApiOperation({ summary }),
+    ApiParam({
+      name: 'id',
+      type: Number,
+      description: '주식 ID',
+      example: 1,
+    }),
+    ApiQuery({
+      name: 'lastStartTime',
+      required: false,
+      description: '마지막 시작 시간 (ISO 8601 형식)',
+      example: '2024-04-01T00:00:00.000Z',
+      type: String,
+      format: 'date-time',
+    }),
+    ApiResponse({
+      status: 200,
+      description: `주식의 ${type} 단위 데이터 성공적으로 조회`,
+      type: StockDataResponse,
+    }),
+    ApiResponse({
+      status: 404,
+      description: '주식 데이터가 존재하지 않음',
+    }),
+  );
+}
