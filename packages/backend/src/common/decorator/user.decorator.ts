aa78d8b00adf1f9cc34ad7d63af2b3f157569ee5
kimminsu <xjfcnfw3@naver.com>
2024-11-13 22:18:15 +0900

    ✨ feat: getUser 데코레이터를 인증된 유저 객체만 가져올 수 있도록 변경

--- packages/backend/src/common/decorator/user.decorator.ts
+++ packages/backend/src/common/decorator/user.decorator.ts
@@ -1,10 +1,17 @@
-import { User } from '@/user/domain/user.entity';
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Request } from 'express';
+import { User } from '@/user/domain/user.entity';
 
 export const GetUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): User => {
     const request: Request = ctx.switchToHttp().getRequest<Request>();
+    if (!request.user) {
+      throw new UnauthorizedException('Unauthorized');
+    }
     return request.user as User;
   },
 );
