b4a1523698dd9207af16f5e1f5401354bfd231c6
baegyeong <beth0929@naver.com>
2024-11-13 22:46:00 +0900

    ✨ feat: metric 정보에 hover 됐을 때 message 출력되도록 구조 변경

--- packages/frontend/src/pages/stock-detail/StockMetricsPanel.tsx
+++ packages/frontend/src/pages/stock-detail/StockMetricsPanel.tsx
@@ -1,17 +1,26 @@
 import { MetricSection, Title } from './components';
-import mockData from '@/mocks/data.json';
+import { Tooltip } from '@/components/ui';
+import { METRIC_DETAILS } from '@/constants/METRIC_DETAILS';
 
 export const StockMetricsPanel = () => {
   return (
     <article className="flex flex-col gap-10 rounded-md bg-white p-6 shadow">
-      <section className="flex items-center gap-10">
-        <Title>거래량</Title>
+      <section className="group flex items-center gap-10">
+        <div className="group relative">
+          <Tooltip className="absolute bottom-full mb-6">
+            {METRIC_DETAILS.tradingVolume.message}
+          </Tooltip>
+          <Title>{METRIC_DETAILS.tradingVolume.name}</Title>
+        </div>
         <span className="display-medium14 text-dark-gray">00.000</span>
       </section>
-      <MetricSection title="가격" metricInfo={mockData.price} />
+      <MetricSection
+        title="가격"
+        metricInfo={Object.values(METRIC_DETAILS.price)}
+      />
       <MetricSection
         title="기업가치"
-        metricInfo={mockData['enterprise value']}
+        metricInfo={Object.values(METRIC_DETAILS.enterpriseValue)}
       />
     </article>
   );
