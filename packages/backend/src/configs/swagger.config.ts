8b33b3f11a0f8c30f454541fa9f5c18ef33726d1
kimminsu <xjfcnfw3@naver.com>
2024-11-14 11:46:39 +0900

    ♻️ refactor: swagger url 변경

--- packages/backend/src/configs/swagger.config.ts
+++ packages/backend/src/configs/swagger.config.ts
@@ -9,5 +9,5 @@ export function useSwagger(app: INestApplication) {
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory);
 }
