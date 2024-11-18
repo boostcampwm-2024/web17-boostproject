4fc0a89a973956b1ba5d030e6f5d64ece139c5f3
baegyeong <beth0929@naver.com>
2024-11-18 20:45:51 +0900

    ✨ feat: 주식 정보를 나타내는 카드 컴포넌트 구현

---
+++ packages/frontend/src/pages/stocks/components/StockInfoCard.tsx
@@ -0,0 +1,47 @@
+import { cn } from '@/utils/cn';
+
+interface StockInfoCardProps {
+  name: string;
+  currentPrice: number;
+  changeRate: number;
+  changeRatePercent: number;
+  index: number;
+}
+
+export const StockInfoCard = ({
+  name,
+  currentPrice,
+  changeRate,
+  changeRatePercent,
+  index,
+}: StockInfoCardProps) => {
+  return (
+    <div
+      className={cn(
+        'flex cursor-pointer flex-col gap-2 rounded-md py-4 pl-5 pr-12 shadow transition-all duration-300 hover:scale-105',
+        index === 0 ? 'bg-light-yellow' : 'bg-white',
+      )}
+    >
+      <p className="display-bold16 text-dark-gray mb-3 text-ellipsis">{name}</p>
+      <div className="flex items-center gap-5">
+        <span className="display-bold12 text-dark-gray">등락률</span>
+        <span
+          className={cn(
+            'display-bold16',
+            changeRate >= 0 ? 'text-red' : 'text-blue',
+          )}
+        >
+          {changeRate >= 0 && '+'}
+          {changeRate.toLocaleString()}원 ({changeRatePercent}
+          %)
+        </span>
+      </div>
+      <div className="flex items-center gap-5">
+        <span className="display-bold12 text-dark-gray">현재가</span>
+        <span className="display-medium12 text-dark-gray">
+          {currentPrice.toLocaleString()}
+        </span>
+      </div>
+    </div>
+  );
+};
