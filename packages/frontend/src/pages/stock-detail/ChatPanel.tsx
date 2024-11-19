8c4930dfc20f062cbdc0a6172da35beea28f9f29
baegyeong <beth0929@naver.com>
2024-11-19 17:25:42 +0900

    🐛 fix: chatPanel 컴포넌트 가운데 정렬 삭제

--- packages/frontend/src/pages/stock-detail/ChatPanel.tsx
+++ packages/frontend/src/pages/stock-detail/ChatPanel.tsx
@@ -2,10 +2,13 @@ import { TextArea } from './components';
 
 export const ChatPanel = () => {
   return (
-    <article className="flex flex-col items-center gap-5 rounded-md bg-white p-7">
+    <article className="flex flex-col gap-5 rounded-md bg-white p-7">
       <h2 className="display-medium20 text-center">채팅</h2>
       <TextArea />
       <hr className="bg-light-gray h-px w-full divide-y border-0"></hr>
+      <section className="flex flex-col gap-5">
+        {/* ChatMessage 컴포넌트 */}
+      </section>
     </article>
   );
 };
