6737e6b660eb8eccd0eafac7c96075b08db773f1
baegyeong <beth0929@naver.com>
2024-11-19 16:15:14 +0900

    🐛 fix: login 전체 네모박스 추가

--- packages/frontend/src/pages/login/Login.tsx
+++ packages/frontend/src/pages/login/Login.tsx
@@ -12,21 +12,23 @@ interface LoginButtonProps {
 export const Login = () => {
   return (
     <div className="flex h-[calc(100vh-8rem)] flex-col items-center justify-center gap-16">
-      <h1 className="display-medium20 text-dark-gray text-center">
-        스마트한 투자의 첫걸음, <br /> 주춤주춤과 함께해요!
-      </h1>
-      <section className="flex flex-col gap-4">
-        <LoginButton to="/" src={google} alt="구글 로그인" />
-        <LoginButton to="/" src={kakao} alt="카카오 로그인" />
-        <LoginButton to="/" src={naver} alt="네이버 로그인" />
-      </section>
+      <main className="flex flex-col gap-16 rounded-md bg-white p-24 shadow-sm">
+        <h1 className="display-medium20 text-dark-gray text-center">
+          스마트한 투자의 첫걸음, <br /> 주춤주춤과 함께해요!
+        </h1>
+        <section className="flex flex-col gap-4">
+          <LoginButton to="/" src={google} alt="구글 로그인" />
+          <LoginButton to="/" src={kakao} alt="카카오 로그인" />
+          <LoginButton to="/" src={naver} alt="네이버 로그인" />
+        </section>
+      </main>
     </div>
   );
 };
 
 export const LoginButton = ({ to, src, alt }: LoginButtonProps) => {
   return (
-    <Link to={to} className="w-72 shadow">
+    <Link to={to} className="w-72">
       <img src={src} alt={alt} />
     </Link>
   );
