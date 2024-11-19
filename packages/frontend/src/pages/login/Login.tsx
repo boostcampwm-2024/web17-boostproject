961cfe548b2e056ee985a2add43225230536553f
baegyeong <beth0929@naver.com>
2024-11-19 18:04:23 +0900

    🐛 fix: 로그인 UI의 뒷배경 추가

--- packages/frontend/src/pages/login/Login.tsx
+++ packages/frontend/src/pages/login/Login.tsx
@@ -11,12 +11,14 @@ interface LoginButtonProps {
 
 export const Login = () => {
   return (
-    <div className="flex h-[calc(100vh-8rem)] flex-col items-center justify-center gap-16">
-      <main className="flex flex-col gap-16 rounded-md bg-white p-24 shadow-sm">
-        <h1 className="display-medium20 text-dark-gray text-center">
-          스마트한 투자의 첫걸음, <br /> 주춤주춤과 함께해요!
-        </h1>
-        <section className="flex flex-col gap-4">
+    <div className="flex h-[calc(100vh-8rem)] flex-col items-center justify-center">
+      <main className="relative flex flex-col gap-32 rounded-lg bg-gradient-to-br from-[#ffe259] to-[#ffa751] p-24 py-32 shadow-sm">
+        <div className="absolute inset-0 rounded-md bg-white/40 backdrop-blur-sm" />
+        <section className="relative z-10">
+          <h2 className="display-bold24">스마트한 투자의 첫걸음,</h2>
+          <p className="display-medium20">주춤주춤과 함께해요!</p>
+        </section>
+        <section className="relative z-10 flex flex-col gap-4">
           <LoginButton to="/" src={google} alt="구글 로그인" />
           <LoginButton to="/" src={kakao} alt="카카오 로그인" />
           <LoginButton to="/" src={naver} alt="네이버 로그인" />
