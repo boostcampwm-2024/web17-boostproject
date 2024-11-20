4dcaa3109ebbf50869e7c8f999de641cf29f89e5
sunghwki <swkim12345@naver.com>
2024-11-21 01:02:23 +0900

    ♻️ refactor: openapi scraper service 에서 안 쓰이는 클래스 삭제

--- packages/backend/src/scraper/openapi/openapi-scraper.service.ts
+++ packages/backend/src/scraper/openapi/openapi-scraper.service.ts
@@ -3,11 +3,9 @@ import { DataSource } from 'typeorm';
 import { OpenapiDetailData } from './api/openapiDetailData.api';
 import { OpenapiMinuteData } from './api/openapiMinuteData.api';
 import { OpenapiPeriodData } from './api/openapiPeriodData.api';
-import { openApiToken } from './api/openapiToken.api';
 
 @Injectable()
 export class OpenapiScraperService {
-  private readonly token = openApiToken;
   public constructor(
     private datasource: DataSource,
     private readonly openapiPeriodData: OpenapiPeriodData,
