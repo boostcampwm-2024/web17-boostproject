356187f63663ea25040ac6554d3af1fb114b667d
baegyeong <beth0929@naver.com>
2024-11-22 00:42:38 +0900

    ✨ feat: 채팅 emit listen 이벤트 설정

--- packages/frontend/src/pages/stock-detail/components/ChatMessage.tsx
+++ packages/frontend/src/pages/stock-detail/components/ChatMessage.tsx
@@ -1,12 +1,19 @@
 import Like from '@/assets/like.svg?react';
+import { cn } from '@/utils/cn';
 
 interface ChatMessageProps {
   name: string;
   contents: string;
-  like: number;
+  likeCount: number;
+  liked: boolean;
 }
 
-export const ChatMessage = ({ name, contents, like }: ChatMessageProps) => {
+export const ChatMessage = ({
+  name,
+  contents,
+  likeCount,
+  liked,
+}: ChatMessageProps) => {
   return (
     <div className="flex">
       <p className="display-bold12 text-dark-gray mr-3 w-12 flex-shrink-0">
@@ -15,8 +22,13 @@ export const ChatMessage = ({ name, contents, like }: ChatMessageProps) => {
       <div>
         <p className="display-medium12 text-dark-gray">{contents}</p>
         <div className="flex items-center gap-1">
-          <Like className="fill-gray hover:fill-orange cursor-pointer" />
-          <span className="display-medium12 text-gray">{like}</span>
+          <Like
+            className={
+              (cn('hover:fill-orange cursor-pointer'),
+              liked ? 'fill-orange' : 'fill-gray')
+            }
+          />
+          <span className="display-medium12 text-gray">{likeCount}</span>
         </div>
       </div>
     </div>
--- packages/frontend/src/pages/stock-detail/components/TextArea.tsx
+++ packages/frontend/src/pages/stock-detail/components/TextArea.tsx
@@ -1,16 +1,31 @@
+import { type FormEvent, useState } from 'react';
 import Send from '@/assets/send.svg?react';
 
-export const TextArea = () => {
+interface TextAreaProps {
+  onSend: (text: string) => void;
+}
+
+export const TextArea = ({ onSend }: TextAreaProps) => {
+  const [chatText, setChatText] = useState('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSend(chatText);
+    setChatText('');
+  };
+
   return (
-    <div className="relative w-full">
+    <form className="relative w-full" onSubmit={(e) => handleSubmit(e)}>
       <textarea
         maxLength={100}
+        value={chatText}
         placeholder="주주 사용자만 입력 가능해요."
         className="display-medium12 bg-light-yellow h-20 w-full resize-none rounded-md p-3 pr-10 focus:outline-none"
-      ></textarea>
+        onChange={(e) => setChatText(e.target.value)}
+      />
       <button className="bg-orange absolute right-2 top-1/2 -translate-y-1/2 rounded p-1">
         <Send />
       </button>
-    </div>
+    </form>
   );
 };
