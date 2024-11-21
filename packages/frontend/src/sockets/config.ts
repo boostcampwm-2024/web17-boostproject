920c83dce03eee01476cda0f2a9069c97c51aafb
baegyeong <beth0929@naver.com>
2024-11-22 01:19:21 +0900

    🐛 fix: 웹소켓 쿼리스트링으로 들어가지 않는 문제 수정

--- packages/frontend/src/sockets/config.ts
+++ packages/frontend/src/sockets/config.ts
@@ -8,14 +8,13 @@ export interface SocketChatType {
 }
 
 export const socketChat = ({ stockId, pageSize = 20 }: SocketChatType) => {
-  return io(`${URL}/api/chat/realtime`, {
-    transports: ['websocket'],
-    reconnectionDelayMax: 10000,
-    query: {
-      stockId,
-      pageSize,
+  return io(
+    `${URL}/api/chat/realtime?stockId=${stockId}&pageSize=${pageSize}`,
+    {
+      transports: ['websocket'],
+      reconnectionDelayMax: 10000,
     },
-  });
+  );
 };
 
 export const socketStock = io(`${URL}/api/stock/realtime`, {
