e55cadd6feb1d00b8cd2473112a3bfec9a509c9c
kimminsu <xjfcnfw3@naver.com>
2024-11-23 21:46:14 +0900

    ✨ feat: 테스터 유저 생성 기능 구현

--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -27,20 +27,23 @@ export class UserService {
     });
   }
 
+  async registerTester() {
+    return await this.dataSource.transaction(async (manager) => {
+      return await manager.save(User, {
+        nickname: this.generateRandomNickname(),
+        email: 'tester@nav',
+        type: OauthType.LOCAL,
+        oauthId: String(
+          (await this.getMaxOauthId(OauthType.LOCAL, manager)) + 1,
+        ),
+      });
+    });
+  }
+
   async findUserByOauthIdAndType(oauthId: string, type: OauthType) {
     return await this.dataSource.manager.findOne(User, {
       where: { oauthId, type },
     });
-  }
-
-  private async validateUserExists(
-    type: OauthType,
-    oauthId: string,
-    manager: EntityManager,
-  ) {
-    if (await manager.exists(User, { where: { oauthId, type } })) {
-      throw new BadRequestException('user already exists');
-    }
   }
 
   async updateUserTheme(userId: number, isLight?: boolean): Promise<User> {
@@ -71,5 +74,54 @@ export class UserService {
     }
 
     return user.isLight;
+  }
+
+  private generateRandomNickname(): string {
+    const adjectives = [
+      '강력한',
+      '지혜로운',
+      '소중한',
+      '빛나는',
+      '고요한',
+      '용감한',
+      '행운의',
+      '신비로운',
+    ];
+    const animals = [
+      '호랑이',
+      '독수리',
+      '용',
+      '사슴',
+      '백호',
+      '하늘새',
+      '백두산 호랑이',
+      '붉은 여우',
+    ];
+
+    const randomAdjective =
+      adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+
+    return `${randomAdjective} ${randomAnimal}`;
+  }
+
+  private async getMaxOauthId(oauthType: OauthType, manager: EntityManager) {
+    const result = await manager
+      .createQueryBuilder(User, 'user')
+      .select('MAX(user.oauthId)', 'max')
+      .where('user.type = :oauthType', { oauthType })
+      .getRawOne();
+
+    return result ? Number(result.max) : 1;
+  }
+
+  private async validateUserExists(
+    type: OauthType,
+    oauthId: string,
+    manager: EntityManager,
+  ) {
+    if (await manager.exists(User, { where: { oauthId, type } })) {
+      throw new BadRequestException('user already exists');
+    }
   }
 }
