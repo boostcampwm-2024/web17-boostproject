41d2c85b63ed3fb09cd5c9681ef481ddff3b4100
sunghwki <swkim12345@naver.com>
2024-11-24 19:31:42 +0900

    ♻️ refactor: web socket client 리팩토링

--- packages/backend/src/scraper/korea-stock-info/openapi/websocketClient.service.ts
+++ packages/backend/src/scraper/korea-stock-info/openapi/websocketClient.service.ts
@@ -4,13 +4,18 @@ import { Cron } from '@nestjs/schedule';
 import { Logger } from 'winston';
 import { WebSocket } from 'ws';
 import { OpenapiLiveData } from './api/openapiLiveData.api';
+import { parseMessage } from './parse/openapi.parser';
+import { openApiToken } from '@/scraper/openapi/api/openapiToken.api';
+import { openApiConfig } from '@/scraper/openapi/config/openapi.config';
 
+type TR_IDS = '0' | '1';
 @Injectable()
 export class WebsocketClient {
   private client: WebSocket;
   private readonly reconnectInterval = 60000;
   private readonly url =
     process.env.WS_URL ?? 'ws://ops.koreainvestment.com:21000';
+  private readonly clientStock: Set<string> = new Set();
 
   constructor(
     @Inject('winston') private readonly logger: Logger,
@@ -22,50 +27,86 @@ export class WebsocketClient {
   }
 
   // TODO : subscribe 구조로 리팩토링
-  private subscribe() {}
-
-  private message(data: any) {
-    this.logger.info(`Received message: ${data}`);
-    if (data.header && data.header.tr_id === 'PINGPONG') {
-      this.logger.info(`Received PING: ${JSON.stringify(data)}`);
-      this.sendPong();
-      return;
-    }
-    if (data.header && data.header.tr_id === 'H0STCNT0') {
-      return;
-    }
-    this.openapiLiveData.output(data);
+  subscribe(stockId: string) {
+    this.clientStock.add(stockId);
+    // TODO : 하나의 config만 사용중.
+    const message = this.convertObjectToMessage(
+      openApiToken.configs[0],
+      stockId,
+      '1',
+    );
+    this.sendMessage(message);
   }
 
-  @Cron('0 2 * * 1-5')
-  private connect() {
-    this.client = new WebSocket(this.url);
+  discribe(stockId: string) {
+    this.clientStock.delete(stockId);
+    const message = this.convertObjectToMessage(
+      openApiToken.configs[0],
+      stockId,
+      '0',
+    );
+    this.sendMessage(message);
+  }
 
-    this.client.on('open', () => {
-      this.logger.info('WebSocket connection established');
-      this.openapiLiveData.getMessage().then((val) => {
-        val.forEach((message) => this.sendMessage(message));
-      });
-    });
-
-    this.client.on('message', (data: any) => {
-      try {
-        this.message(data);
-      } catch (error) {
-        this.logger.info(error);
-      }
-    });
-
+  private initDisconnect() {
     this.client.on('close', () => {
       this.logger.warn(
         `WebSocket connection closed. Reconnecting in ${this.reconnectInterval / 60 / 1000} minute...`,
       );
-      setTimeout(() => this.connect(), this.reconnectInterval);
     });
 
     this.client.on('error', (error: any) => {
       this.logger.error(`WebSocket error: ${error.message}`);
+      setTimeout(() => this.connect(), this.reconnectInterval);
     });
+  }
+
+  private initOpen() {
+    this.client.on('open', () => {
+      this.logger.info('WebSocket connection established');
+      for (const stockId of this.clientStock.keys()) {
+        const message = this.convertObjectToMessage(
+          openApiToken.configs[0],
+          stockId,
+          '1',
+        );
+        this.sendMessage(message);
+      }
+    });
+  }
+
+  private initMessage() {
+    this.client.on('message', async (data) => {
+      try {
+        let message;
+        if (typeof data === 'object') {
+          message = data;
+        } else {
+          message = parseMessage(data as string);
+        }
+        if (message.header && message.header.tr_id === 'PINGPONG') {
+          this.logger.info(`Received PING: ${JSON.stringify(data)}`);
+          this.sendPong();
+          return;
+        }
+        if (message.header && message.header.tr_id === 'H0STCNT0') {
+          return;
+        }
+        this.logger.info(`Recived data : ${data}`);
+        const liveData = this.openapiLiveData.convertLiveData(message);
+        this.openapiLiveData.saveLiveData(liveData);
+      } catch (error) {
+        this.logger.warn(error);
+      }
+    });
+  }
+
+  @Cron('0 2 * * 1-5')
+  connect() {
+    this.client = new WebSocket(this.url);
+    this.initOpen();
+    this.initMessage();
+    this.initDisconnect();
   }
 
   private sendPong() {
@@ -74,6 +115,28 @@ export class WebsocketClient {
     };
     this.client.send(JSON.stringify(pongMessage));
     this.logger.info(`Sent PONG: ${JSON.stringify(pongMessage)}`);
+  }
+
+  private convertObjectToMessage(
+    config: typeof openApiConfig,
+    stockId: string,
+    tr_type: TR_IDS,
+  ): string {
+    const message = {
+      header: {
+        approval_key: config.STOCK_WEBSOCKET_KEY!,
+        custtype: 'P',
+        tr_type,
+        'content-type': 'utf-8',
+      },
+      body: {
+        input: {
+          tr_id: 'H0STCNT0',
+          tr_key: stockId,
+        },
+      },
+    };
+    return JSON.stringify(message);
   }
 
   private sendMessage(message: string) {
