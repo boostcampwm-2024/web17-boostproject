2e795893fd02bf3010988b9b96bb015fc9f5479e
baegyeong <beth0929@naver.com>
2024-11-25 22:07:21 +0900

    ✨ feat: 사이드바의 알림창 구현

--- packages/frontend/src/components/layouts/Sidebar.tsx
+++ packages/frontend/src/components/layouts/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import logoCharacter from '/logoCharacter.png';
 import logoTitle from '/logoTitle.png';
+import { Alarm } from './alarm';
 import { MenuList } from './MenuList';
 import { Search } from './search';
 import { BOTTOM_MENU_ITEMS, TOP_MENU_ITEMS } from '@/constants/menuItems';
@@ -11,16 +12,28 @@ import { cn } from '@/utils/cn';
 export const Sidebar = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
+  const [showAlarm, setShowAlarm] = useState(false);
 
   const ref = useOutsideClick(() => {
     if (showSearch) {
       setShowSearch(false);
     }
+
+    if (showAlarm) {
+      setShowAlarm(false);
+    }
   });
 
   const handleMenuItemClick = (item: MenuSection) => {
+    console.log(item.text);
     if (item.text === '검색') {
-      setShowSearch((prev) => !prev);
+      setShowSearch(true);
+      setShowAlarm(false);
+    }
+
+    if (item.text === '알림') {
+      setShowSearch(false);
+      setShowAlarm(true);
     }
   };
 
@@ -68,6 +81,7 @@ export const Sidebar = () => {
         )}
       >
         {showSearch && <Search className="h-screen" />}
+        {showAlarm && <Alarm className="h-screen" />}
       </div>
     </div>
   );
