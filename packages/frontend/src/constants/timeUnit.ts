13c68c0e24810b6ebd850760025fedd2d9701913
baegyeong <beth0929@naver.com>
2024-11-26 04:04:41 +0900

    ✨ feat: 시간단위를 선택하여 상태관리하는 버튼 추가

---
+++ packages/frontend/src/constants/timeUnit.ts
@@ -0,0 +1,12 @@
+import { StockTimeSeriesRequest } from '@/apis/queries/stocks';
+
+export const TIME_UNIT: Array<{
+  id: number;
+  time: StockTimeSeriesRequest['timeUnit'];
+  label: string;
+}> = [
+  { id: 1, time: 'day', label: '일' },
+  { id: 2, time: 'week', label: '주' },
+  { id: 3, time: 'month', label: '월' },
+  { id: 4, time: 'year', label: '년' },
+];
