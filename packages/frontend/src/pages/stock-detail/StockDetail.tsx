38b5deba252c71bf081f006be7dd77c965f6760a
baegyeong <beth0929@naver.com>
2024-11-26 22:29:25 +0900

    ✨ feat: 유저의 상태에 따라 주식 추가 삭제 분기 처리

--- packages/frontend/src/pages/stock-detail/StockDetail.tsx
+++ packages/frontend/src/pages/stock-detail/StockDetail.tsx
@@ -1,3 +1,5 @@
+import { useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { useParams } from 'react-router-dom';
 import {
   AddAlarmForm,
@@ -6,22 +8,64 @@ import {
   StockMetricsPanel,
   TradingChart,
 } from '.';
-import { useGetStockDetail } from '@/apis/queries/stock-detail';
+import { useGetLoginStatus } from '@/apis/queries/auth';
+import {
+  useGetOwnership,
+  useGetStockDetail,
+  usePostStockUser,
+} from '@/apis/queries/stock-detail';
 import Plus from '@/assets/plus.svg?react';
 import { Button } from '@/components/ui/button';
+import { Modal } from '@/components/ui/modal';
+import { modalMessage, ModalMessage } from '@/constants/modalMessage';
 
 export const StockDetail = () => {
   const { stockId } = useParams();
-  const { data } = useGetStockDetail({ stockId: stockId ?? '' });
+  const [showModal, setShowModal] = useState(false);
+
+  const { data: stockDetail } = useGetStockDetail({ stockId: stockId ?? '' });
+  const { data: loginStatus } = useGetLoginStatus();
+  const { data: userOwnerStock } = useGetOwnership({ stockId: stockId ?? '' });
+  const { mutate, isSuccess } = usePostStockUser();
+
+  const userStatus: ModalMessage = useMemo(() => {
+    if (loginStatus?.message === 'Not Authenticated') {
+      return 'NOT_AUTHENTICATED';
+    }
+
+    return userOwnerStock?.isOwner ? 'OWNERSHIP' : 'NOT_OWNERSHIP';
+  }, [loginStatus?.message, userOwnerStock?.isOwner]);
 
   return (
     <div className="flex h-full flex-col gap-7">
       <header className="flex gap-7">
-        <h1 className="display-bold24">{data?.name}</h1>
-        <Button className="flex items-center justify-center gap-1">
-          <Plus /> 내 주식 추가
+        <h1 className="display-bold24">{stockDetail?.name}</h1>
+        <Button
+          className="flex items-center justify-center gap-1"
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          <Plus /> {modalMessage[userStatus].label}
         </Button>
       </header>
+      {showModal &&
+        createPortal(
+          <Modal
+            title="주식 소유"
+            onClose={() => setShowModal(false)}
+            onConfirm={() => {
+              mutate({ stockId: stockId ?? '' });
+              if (isSuccess) {
+                setShowModal(false);
+              }
+            }}
+            isShowButton={modalMessage[userStatus].button}
+          >
+            {modalMessage[userStatus].message}
+          </Modal>,
+          document.body,
+        )}
       <article className="grid flex-1 grid-cols-[2.5fr_1fr_1fr] gap-5 [&_section]:gap-5">
         <section className="flex flex-col">
           <div className="relative h-full">
