a5d08434e98ec6518b1e2929411ffbf4045708b9
baegyeong <beth0929@naver.com>
2024-11-26 12:29:09 +0900

    ✨ feat: 검색 결과에 따른 렌더링 처리

---
+++ packages/frontend/src/components/layouts/search/SearchResults.tsx
@@ -0,0 +1,47 @@
+import Lottie from 'react-lottie-player';
+import { Link } from 'react-router-dom';
+import { type SearchResultsResponse } from '@/apis/queries/stocks';
+import loading from '@/components/lottie/loading-animation.json';
+
+interface SearchResultsProps {
+  data?: SearchResultsResponse;
+  isLoading: boolean;
+  isError: boolean;
+}
+
+export const SearchResults = ({
+  data,
+  isLoading,
+  isError,
+}: SearchResultsProps) => {
+  if (isLoading) {
+    return (
+      <Lottie
+        className="fill-orange h-52 w-52"
+        animationData={loading}
+        play
+        speed={3}
+      />
+    );
+  }
+
+  if (isError) {
+    return <p className="text-dark-gray">검색 결과가 없어요.</p>;
+  }
+
+  if (data) {
+    return (
+      <ul className="flex flex-col gap-2">
+        {data.searchResults.map((stock) => (
+          <Link
+            to={`/stocks/${stock.id}`}
+            key={stock.id}
+            className="text-dark-gray hover:text-orange leading-7 hover:underline"
+          >
+            {stock.name}
+          </Link>
+        ))}
+      </ul>
+    );
+  }
+};
