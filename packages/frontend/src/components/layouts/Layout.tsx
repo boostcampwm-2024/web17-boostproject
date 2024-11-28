d365be38ede97b4716e22662e7000f6f6e9557b3
baegyeong <beth0929@naver.com>
2024-11-28 16:04:20 +0900

    🐛 fix: 반응형 전체 레이아웃 padding 수정

--- packages/frontend/src/components/layouts/Layout.tsx
+++ packages/frontend/src/components/layouts/Layout.tsx
@@ -6,7 +6,7 @@ export const Layout = () => {
     <div className="flex min-h-screen">
       <Sidebar />
       <main className="ml-20 flex-1">
-        <div className="h-full overflow-auto px-16 py-16 md:px-32 lg:px-48">
+        <div className="h-full overflow-auto px-16 py-16 md:px-24 lg:px-28 xl:px-44">
           <Outlet />
         </div>
       </main>
