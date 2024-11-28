82b54f2d48accb0d810549c59eb70c2d9df39091
baegyeong <beth0929@naver.com>
2024-11-28 18:55:23 +0900

    🐛 fix: 주식 정보 카드 스타일 수정

--- packages/frontend/src/pages/stocks/components/StockInfoCard.tsx
+++ packages/frontend/src/pages/stocks/components/StockInfoCard.tsx
@@ -16,13 +16,13 @@ export const StockInfoCard = ({
   return (
     <div
       className={cn(
-        'flex cursor-pointer flex-col gap-2 rounded-md p-2 shadow transition-all duration-300 hover:scale-105 lg:py-4 lg:pl-5 lg:pr-12',
+        'flex cursor-pointer flex-col gap-2 rounded-md p-2 shadow transition-all duration-300 hover:scale-105 lg:py-4 lg:pl-5 lg:pr-16',
         index === 0 ? 'bg-light-yellow' : 'bg-white',
       )}
       onClick={onClick}
     >
       <p className="display-bold16 text-dark-gray mb-3">{name}</p>
-      <section className="flex flex-row gap-3 xl:flex-col">
+      <section className="flex flex-row gap-2 xl:flex-col">
         <div className="flex flex-col items-start gap-2 xl:flex-row xl:items-center xl:gap-5">
           <span className="display-bold12 text-dark-gray">등락률</span>
           <span
