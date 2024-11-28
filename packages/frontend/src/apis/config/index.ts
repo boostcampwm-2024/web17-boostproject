551bacd49e0eb4b12bc97fa153f90efb6832f182
baegyeong <beth0929@naver.com>
2024-11-28 13:46:00 +0900

    ✨ feat: axios interceptor 추가

--- packages/frontend/src/apis/config/index.ts
+++ packages/frontend/src/apis/config/index.ts
@@ -1,7 +1,24 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  timeout: 1000,
+  timeout: 10000,
   withCredentials: true,
 });
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const status = error.response?.status;
+
+    if (status === 400) {
+      alert('잘못된 요청입니다.');
+    }
+
+    if (status === 403) {
+      alert('로그인이 필요합니다.');
+      location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
