4f54cec10614887f3a7b1020fd1d18ecf096e48d
sunghwki <swkim12345@naver.com>
2024-11-29 11:45:28 +0900

    💄 style: 테스트 코드 삭제

--- test.js
+++
@@ -1,7 +0,0 @@
-const webPush = require('web-push');
-
-// VAPID 키 쌍 생성
-const vapidKeys = webPush.generateVAPIDKeys();
-
-console.log('Public Key:', vapidKeys.publicKey);
-console.log('Private Key:', vapidKeys.privateKey);
