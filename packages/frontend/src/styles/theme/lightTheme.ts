f4cf77d6bfeedc5feff2c4f38efc06f78dfd47b1
baegyeong <beth0929@naver.com>
2024-12-01 00:04:37 +0900

    🐛 fix: 그래프 라이브러리에서 var 속성을 인식하지 못하는 문제 수정

--- packages/frontend/src/styles/theme/lightTheme.ts
+++ packages/frontend/src/styles/theme/lightTheme.ts
@@ -1,19 +1,15 @@
-import resolveConfig from 'tailwindcss/resolveConfig';
 import { ChartTheme } from '.';
-import tailwindConfig from '@/../tailwind.config';
-
-const colorConfig = resolveConfig(tailwindConfig).theme.colors;
 
 export const lightTheme: ChartTheme = {
-  background: colorConfig.white,
+  background: '#ffffff',
   textColor: '#000000e6',
   gridLines: '#e0e3eb',
   borderColor: '#d6dcde',
   candlestick: {
-    upColor: colorConfig.red.toString(),
-    downColor: colorConfig.blue.toString(),
-    borderUpColor: colorConfig.red.toString(),
-    borderDownColor: colorConfig.blue.toString(),
+    upColor: '#ff4d4d',
+    downColor: '#1a75ff',
+    borderUpColor: '#ff4d4d',
+    borderDownColor: '#1a75ff',
     wickUpColor: '#737375',
     wickDownColor: '#737375',
   },
