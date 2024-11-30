cb4a2ce01d9a3498465a1a0298b6d208b3a6ddb8
sunghwki <swkim12345@naver.com>
2024-11-30 12:06:25 +0900

    📝 docs: alarm request swagger 추가

--- packages/backend/src/alarm/dto/alarm.request.ts
+++ packages/backend/src/alarm/dto/alarm.request.ts
@@ -1,6 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class AlarmRequest {
+  @ApiProperty({
+    description: '주식 아이디',
+    example: '005930',
+  })
   stock_id: string;
+
+  @ApiProperty({
+    description: '목표 가격',
+    example: 150.0,
+    required: false,
+  })
   targetPrice?: number;
+
+  @ApiProperty({
+    description: '목표 거래량',
+    example: 1000,
+    required: false,
+  })
   targetVolum?: number;
+
+  @ApiProperty({
+    description: '알림 날짜',
+    example: '2023-10-01T00:00:00Z',
+    required: false,
+  })
   alarmDate?: Date;
 }
