ec589ea38410c2166299e811ed7fa3d627db21bc
sunghwki <swkim12345@naver.com>
2024-12-01 19:58:26 +0900

    🐛 fix: subscriber 조건 변경

--- packages/backend/src/alarm/alarm.subscriber.ts
+++ packages/backend/src/alarm/alarm.subscriber.ts
@@ -27,14 +27,31 @@ export class AlarmSubscriber
     return StockMinutely;
   }
 
+  isValidAlarm(alarm: Alarm, entity: StockMinutely) {
+    if (alarm.alarmDate && alarm.alarmDate > entity.createdAt) {
+      return false;
+    } else {
+      if (alarm.targetPrice && alarm.targetPrice >= entity.open) {
+        return true;
+      }
+      if (alarm.targetVolume && alarm.targetVolume >= entity.volume) {
+        return true;
+      }
+      return false;
+    }
+  }
+
   async afterInsert(event: InsertEvent<StockMinutely>) {
     try {
-      const stockLiveData = event.entity;
-      const alarms = await this.datasource.manager.find(Alarm, {
-        where: { stock: { id: stockLiveData.stock.id } },
+      const stockMinutely = event.entity;
+      const rawAlarms = await this.datasource.manager.find(Alarm, {
+        where: { stock: { id: stockMinutely.stock.id } },
         relations: ['user', 'stock'],
       });
 
+      const alarms = rawAlarms.filter((val) =>
+        this.isValidAlarm(val, stockMinutely),
+      );
       for (const alarm of alarms) {
         await this.alarmService.sendPushNotification(alarm);
       }
