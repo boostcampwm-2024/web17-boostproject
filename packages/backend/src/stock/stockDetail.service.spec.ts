19f009e3321e7f04f40463c78fdc80bec1af8439
kimminsu <xjfcnfw3@naver.com>
2024-12-01 13:02:07 +0900

    🐛 fix: stockDetail 조회 트랜잭션 내부에 또다른 트랜잭션 제거

--- packages/backend/src/stock/stockDetail.service.spec.ts
+++ packages/backend/src/stock/stockDetail.service.spec.ts
@@ -1,75 +1,80 @@
 import { NotFoundException } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { anyString, anything, instance, mock, verify, when } from 'ts-mockito';
+import {
+  DataSource,
+  EntityManager,
+  Repository,
+  SelectQueryBuilder,
+} from 'typeorm';
 import { Logger } from 'winston';
 import { StockDetail } from './domain/stockDetail.entity';
 import { StockDetailService } from './stockDetail.service';
-import { createDataSourceMock } from '@/user/user.service.spec';
-
-const logger: Logger = {
-  error: jest.fn(),
-  warn: jest.fn(),
-  info: jest.fn(),
-} as unknown as Logger;
+import { Stock } from '@/stock/domain/stock.entity';
+import { StockDetailResponse } from '@/stock/dto/stockDetail.response';
 
 describe('StockDetailService 테스트', () => {
   const stockId = 'A005930';
+  let stockDetailService: StockDetailService;
+  let logger: Logger;
+  let dataSourceMock: DataSource;
+  let managerMock: EntityManager;
+  let repositoryMock: Repository<StockDetail>;
+  let queryBuilderMock: SelectQueryBuilder<StockDetail>;
+
+  beforeEach(() => {
+    dataSourceMock = mock(DataSource);
+    logger = mock(Logger);
+    managerMock = mock(EntityManager);
+    repositoryMock = mock(Repository);
+    queryBuilderMock = mock(SelectQueryBuilder);
+    stockDetailService = new StockDetailService(
+      instance(dataSourceMock),
+      instance(logger),
+    );
+    when(dataSourceMock.transaction(anything())).thenCall(async (callback) => {
+      return await callback(instance(managerMock));
+    });
+  });
 
   test('stockId로 주식 상세 정보를 조회한다.', async () => {
-    const mockStockDetail = {
-      stock: { id: stockId },
-      marketCap: 352510000000000,
+    const data = {
+      id: 1,
+      stock: { id: stockId } as Stock,
+      marketCap: String(352510000000000),
       eps: 4091,
       per: 17.51,
       high52w: 88000,
       low52w: 53000,
+      updatedAt: new Date(),
     };
-    const managerMock = {
-      existsBy: jest.fn().mockResolvedValue(true),
-      findBy: jest.fn().mockResolvedValue([mockStockDetail]),
-    };
-    const dataSource = createDataSourceMock(managerMock);
-    const stockDetailService = new StockDetailService(
-      dataSource as DataSource,
-      logger,
+    when(managerMock.existsBy(StockDetail, anything())).thenResolve(true);
+    when(managerMock.getRepository(StockDetail)).thenReturn(
+      instance(repositoryMock),
     );
+    when(repositoryMock.createQueryBuilder(anything())).thenReturn(
+      instance(queryBuilderMock),
+    );
+    when(queryBuilderMock.where(anyString(), anything())).thenReturn(
+      instance(queryBuilderMock),
+    );
+    when(
+      queryBuilderMock.leftJoinAndSelect(anyString(), anyString()),
+    ).thenReturn(instance(queryBuilderMock));
+    when(queryBuilderMock.getOne()).thenResolve(data);
 
     const result = await stockDetailService.getStockDetailByStockId(stockId);
 
-    expect(managerMock.existsBy).toHaveBeenCalledWith(StockDetail, {
-      stock: { id: stockId },
-    });
-    expect(managerMock.findBy).toHaveBeenCalledWith(StockDetail, {
-      stock: { id: stockId },
-    });
-    expect(result).toMatchObject({
-      marketCap: expect.any(Number),
-      eps: expect.any(Number),
-      per: expect.any(Number),
-      high52w: expect.any(Number),
-      low52w: expect.any(Number),
-    });
-    expect(result.marketCap).toEqual(mockStockDetail.marketCap);
-    expect(result.eps).toEqual(mockStockDetail.eps);
-    expect(result.per).toEqual(mockStockDetail.per);
-    expect(result.high52w).toEqual(mockStockDetail.high52w);
-    expect(result.low52w).toEqual(mockStockDetail.low52w);
+    verify(managerMock.existsBy(StockDetail, anything())).once();
+    verify(queryBuilderMock.getOne()).once();
+
+    expect(result).toEqual(new StockDetailResponse(data));
   });
 
   test('존재하지 않는 stockId로 조회 시 예외를 발생시킨다.', async () => {
-    const managerMock = {
-      existsBy: jest.fn().mockResolvedValue(false),
-    };
-    const dataSource = createDataSourceMock(managerMock);
-    const stockDetailService = new StockDetailService(
-      dataSource as DataSource,
-      logger,
-    );
+    when(managerMock.existsBy(StockDetail, anything())).thenResolve(false);
 
     await expect(
       stockDetailService.getStockDetailByStockId('nonexistentId'),
     ).rejects.toThrow(NotFoundException);
-    expect(logger.warn).toHaveBeenCalledWith(
-      `stock detail not found (stockId: nonexistentId)`,
-    );
   });
 });
