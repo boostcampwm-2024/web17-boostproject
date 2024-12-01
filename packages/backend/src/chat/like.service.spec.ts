46a34efe9112fbc2c3615a85aa5a2bbe947ba8af
kimminsu <xjfcnfw3@naver.com>
2024-12-01 13:34:55 +0900

    ✅ test: 좋아요 테스트 mockito 적용

--- packages/backend/src/chat/like.service.spec.ts
+++ packages/backend/src/chat/like.service.spec.ts
@@ -1,18 +1,19 @@
-import { DataSource } from 'typeorm';
+import { DataSource, EntityManager } from 'typeorm';
 import { Chat } from '@/chat/domain/chat.entity';
 import { Like } from '@/chat/domain/like.entity';
 import { LikeService } from '@/chat/like.service';
-import { Stock } from '@/stock/domain/stock.entity';
 import { User } from '@/user/domain/user.entity';
-import { createDataSourceMock } from '@/user/user.service.spec';
+import { anything, instance, mock, when } from 'ts-mockito';
+import { Stock } from '@/stock/domain/stock.entity';
 
 function createChat(): Chat {
   return {
-    stock: new Stock(),
+    stock: { id: '005930', name: '삼성전자' } as Stock,
     user: new User(),
     id: 1,
     likeCount: 1,
     message: '안녕하세요',
+    mentions: [],
     type: 'NORMAL',
     date: {
       createdAt: new Date(),
@@ -22,27 +23,36 @@ function createChat(): Chat {
 }
 
 describe('LikeService 테스트', () => {
-  test('존재하지 않는 채팅을 좋아요를 시도하면 예외가 발생한다.', () => {
-    const managerMock = {
-      findOne: jest.fn().mockResolvedValue(null),
-    };
-    const datasource = createDataSourceMock(managerMock);
-    const likeService = new LikeService(datasource as DataSource);
+  let likeService: LikeService;
+  let datasourceMock: DataSource;
+  let managerMock: EntityManager;
 
-    expect(likeService.toggleLike(1, 1)).rejects.toThrow('Chat not found');
+  test('존재하지 않는 채팅을 좋아요를 시도하면 예외가 발생한다.', async () => {
+    datasourceMock = mock(DataSource);
+    managerMock = mock(EntityManager);
+    when(managerMock.findOne(Chat, anything())).thenResolve(null);
+    when(datasourceMock.transaction(anything())).thenCall(async (callback) => {
+      return await callback(instance(managerMock));
+    });
+
+    likeService = new LikeService(instance(datasourceMock));
+
+    await expect(() => likeService.toggleLike(1, 1)).rejects.toThrow(
+      'Chat not found',
+    );
   });
 
   test('특정 채팅에 좋아요를 한다.', async () => {
     const chat = createChat();
-    const managerMock = {
-      findOne: jest
-        .fn()
-        .mockResolvedValueOnce(chat)
-        .mockResolvedValueOnce(null),
-      save: jest.fn(),
-    };
-    const datasource = createDataSourceMock(managerMock);
-    const likeService = new LikeService(datasource as DataSource);
+    datasourceMock = mock(DataSource);
+    managerMock = mock(EntityManager);
+    when(managerMock.findOne(Chat, anything()))
+      .thenResolve(chat)
+      .thenResolve(null);
+    when(datasourceMock.transaction(anything())).thenCall(async (callback) => {
+      return await callback(instance(managerMock));
+    });
+    likeService = new LikeService(instance(datasourceMock));
 
     const response = await likeService.toggleLike(1, 1);
 
@@ -51,15 +61,14 @@ describe('LikeService 테스트', () => {
 
   test('특정 채팅에 좋아요를 취소한다.', async () => {
     const chat = createChat();
-    const managerMock = {
-      findOne: jest
-        .fn()
-        .mockResolvedValueOnce(chat)
-        .mockResolvedValueOnce(new Like()),
-      remove: jest.fn(),
-    };
-    const datasource = createDataSourceMock(managerMock);
-    const likeService = new LikeService(datasource as DataSource);
+    datasourceMock = mock(DataSource);
+    managerMock = mock(EntityManager);
+    when(managerMock.findOne(Chat, anything())).thenResolve(chat);
+    when(managerMock.findOne(Like, anything())).thenResolve(new Like());
+    when(datasourceMock.transaction(anything())).thenCall(async (callback) => {
+      return await callback(instance(managerMock));
+    });
+    likeService = new LikeService(instance(datasourceMock));
 
     const response = await likeService.toggleLike(1, 1);
 
