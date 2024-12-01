b5147a32775176ff0c8b33144d3968f03f830367
baegyeong <beth0929@naver.com>
2024-12-02 03:23:38 +0900

    ✨ feat: 다크모드 get, patch 스키마 및 api 정의

---
+++ packages/frontend/src/apis/queries/user/usePatchUserTheme.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  type PatchUserTheme,
+  type PatchUserThemeRequest,
+  UserThemeSchema,
+} from './schema';
+import { patch } from '@/apis/utils/patch';
+
+const patchUserTheme = ({ theme }: PatchUserThemeRequest) =>
+  patch<PatchUserTheme>({
+    params: { theme },
+    schema: UserThemeSchema,
+    url: '/api/user/theme',
+  });
+
+export const usePatchUserTheme = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ['patchTheme'],
+    mutationFn: ({ theme }: PatchUserThemeRequest) => patchUserTheme({ theme }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['userTheme'] });
+    },
+  });
+};
