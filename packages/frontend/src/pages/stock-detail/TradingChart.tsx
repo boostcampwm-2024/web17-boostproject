b61b1de7e7274910c7529d9975e53daee059221f
baegyeong <beth0929@naver.com>
2024-12-03 03:23:27 +0900

    ✨ feat: 그래프 스크롤 시 새로운 시간 데이터를 받아오도록 설정

--- packages/frontend/src/pages/stock-detail/TradingChart.tsx
+++ packages/frontend/src/pages/stock-detail/TradingChart.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { type LogicalRange } from 'lightweight-charts';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { RadioButton } from './components/RadioButton';
 import { useChart } from './hooks/useChart';
@@ -13,18 +14,49 @@ export const TradingChart = () => {
   const { stockId } = useParams();
   const [timeunit, setTimeunit] =
     useState<StockTimeSeriesRequest['timeunit']>('day');
+  const isPending = useRef(false);
+
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const { data } = useGetStocksPriceSeries({
+  const { data, fetchNextPage, hasNextPage } = useGetStocksPriceSeries({
     stockId: stockId ?? '',
     timeunit,
   });
 
+  const allPriceData = data?.pages.flatMap((page) => page.priceDtoList) ?? [];
+  const allVolumeData = data?.pages.flatMap((page) => page.volumeDtoList) ?? [];
+
   const chart = useChart({
-    priceData: data?.priceDtoList ?? [],
-    volumeData: data?.volumeDtoList ?? [],
+    priceData: allPriceData,
+    volumeData: allVolumeData,
     containerRef,
   });
+
+  const fetchGraphData = useCallback(
+    async (logicalRange: LogicalRange | null) => {
+      if (isPending.current) return;
+      isPending.current = true;
+
+      if (logicalRange && logicalRange.from < 10 && hasNextPage) {
+        await fetchNextPage();
+      }
+
+      isPending.current = false;
+    },
+    [hasNextPage, fetchNextPage],
+  );
+
+  useEffect(() => {
+    const chartInstance = chart.current;
+    if (!chartInstance) return;
+
+    const timeScale = chartInstance.timeScale();
+    timeScale.subscribeVisibleLogicalRangeChange(fetchGraphData);
+
+    return () => {
+      timeScale.unsubscribeVisibleLogicalRangeChange(fetchGraphData);
+    };
+  }, [chart, fetchGraphData]);
 
   useChartResize({ containerRef, chart });
 
