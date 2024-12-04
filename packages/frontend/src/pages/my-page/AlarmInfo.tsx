0662aa0177fb3edecfa3428ccd13410ea5a18526
baegyeong <beth0929@naver.com>
2024-12-04 22:46:31 +0900

    ✨ feat: 마이페이지에 알림 정보 추가 및 리팩토링

--- packages/frontend/src/pages/my-page/AlarmInfo.tsx
+++ packages/frontend/src/pages/my-page/AlarmInfo.tsx
@@ -1,27 +1,47 @@
+import { useGetAlarm } from '@/apis/queries/alarm';
 import { GetLoginStatus } from '@/apis/queries/auth/schema';
-import { Alarm, AlarmProps } from '@/components/ui/alarm';
-import mock from '@/mocks/alarm.json';
+import { Alarm } from '@/components/ui/alarm';
 
 interface AlarmInfoProps {
   loginStatus: GetLoginStatus;
 }
 
 export const AlarmInfo = ({ loginStatus }: AlarmInfoProps) => {
-  const { goalPrice, method, date } = mock.data[0] as AlarmProps;
-
   return (
     <section className="display-bold20 flex flex-col gap-5 rounded-md bg-white p-7">
       <h2>알림</h2>
-      {loginStatus?.message === 'Authenticated' ? (
-        <>
-          <Alarm goalPrice={goalPrice} method={method} date={date} />
-          <Alarm goalPrice={goalPrice} method={method} date={date} />
-        </>
-      ) : (
-        <p className="display-medium14 text-dark-gray">
-          로그인 후 이용 가능합니다.
-        </p>
-      )}
+      <AlarmInfoContents loginStatus={loginStatus} />
     </section>
   );
+};
+
+const AlarmInfoContents = ({ loginStatus }: AlarmInfoProps) => {
+  const { data } = useGetAlarm({
+    loginStatus: loginStatus.message === 'Authenticated',
+  });
+
+  if (!loginStatus || loginStatus.message === 'Not Authenticated') {
+    return (
+      <p className="text-dark-gray display-medium14">
+        로그인 후 이용 가능해요.
+      </p>
+    );
+  }
+
+  if (!data || data?.length === 0) {
+    return (
+      <p className="text-dark-gray display-medium14">
+        현재 설정된 알림이 없어요.
+      </p>
+    );
+  }
+
+  return data.map((alarm) => (
+    <Alarm
+      key={alarm.alarmId}
+      option={alarm.targetPrice ? '목표가' : '거래가'}
+      goalPrice={alarm.targetPrice ?? alarm.targetVolume!}
+      alarmDate={alarm.alarmDate}
+    />
+  ));
 };
--- packages/frontend/src/pages/my-page/StockInfo.tsx
+++ packages/frontend/src/pages/my-page/StockInfo.tsx
@@ -1,4 +1,3 @@
-import { useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { GetLoginStatus } from '@/apis/queries/auth/schema';
 import { useDeleteStockUser } from '@/apis/queries/stock-detail';
@@ -10,50 +9,57 @@ interface StockInfoProps {
 }
 
 export const StockInfo = ({ loginStatus }: StockInfoProps) => {
-  const queryClient = useQueryClient();
-  const navigate = useNavigate();
-
-  const { data } = useGetUserStock();
-  const { mutate } = useDeleteStockUser({
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['userStock'] }),
-  });
-
-  const { userStocks } = data || {};
-
   return (
     <section className="display-bold20 flex flex-col gap-5 rounded-md bg-white p-7">
       <h2>주식 정보</h2>
-      {loginStatus?.message === 'Authenticated' ? (
-        userStocks?.length === 0 ? (
-          <p className="display-medium14 text-dark-gray">
-            소유한 주식이 없습니다.
-          </p>
-        ) : (
-          <article className="grid gap-5 xl:grid-cols-2">
-            {data?.userStocks.map((stock) => (
-              <section
-                className="display-bold14 text-dark-gray bg-extra-light-gray flex cursor-pointer items-center justify-between rounded px-4 py-2 transition-all duration-300 hover:scale-105 xl:p-8"
-                onClick={() => navigate(`/stocks/${stock.stockId}`)}
-              >
-                <p>{stock.name}</p>
-                <Button
-                  size="sm"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    mutate({ stockId: stock.stockId });
-                  }}
-                >
-                  삭제
-                </Button>
-              </section>
-            ))}
-          </article>
-        )
-      ) : (
-        <p className="display-medium14 text-dark-gray">
-          로그인 후 이용 가능합니다.
-        </p>
-      )}
+      <StockInfoContents loginStatus={loginStatus} />
     </section>
+  );
+};
+
+const StockInfoContents = ({ loginStatus }: StockInfoProps) => {
+  const navigate = useNavigate();
+
+  const { data } = useGetUserStock();
+  const { mutate } = useDeleteStockUser();
+
+  const { userStocks } = data || {};
+
+  if (!loginStatus || loginStatus.message === 'Not Authenticated') {
+    return (
+      <p className="text-dark-gray display-medium14">
+        로그인 후 이용 가능해요.
+      </p>
+    );
+  }
+
+  if (userStocks?.length === 0) {
+    return (
+      <p className="text-dark-gray display-medium14">
+        현재 소유한 주식이 없어요.
+      </p>
+    );
+  }
+
+  return (
+    <article className="grid gap-5 xl:grid-cols-2">
+      {data?.userStocks.map((stock) => (
+        <section
+          className="display-bold14 text-dark-gray bg-extra-light-gray flex cursor-pointer items-center justify-between rounded px-4 py-2 transition-all duration-300 hover:scale-105 xl:p-8"
+          onClick={() => navigate(`/stocks/${stock.stockId}`)}
+        >
+          <p>{stock.name}</p>
+          <Button
+            size="sm"
+            onClick={(e) => {
+              e.stopPropagation();
+              mutate({ stockId: stock.stockId });
+            }}
+          >
+            삭제
+          </Button>
+        </section>
+      ))}
+    </article>
   );
 };
