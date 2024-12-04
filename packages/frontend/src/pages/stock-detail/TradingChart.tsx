5b24de67065f829ec2a242b3eebeec869dcce911
baegyeong <beth0929@naver.com>
2024-12-04 16:53:05 +0900

    🐛 fix: 그래프, 주식 정보의 높이가 옆의 컴포넌트와 맞지 않는 문제 수정

--- packages/frontend/src/pages/stock-detail/TradingChart.tsx
+++ packages/frontend/src/pages/stock-detail/TradingChart.tsx
@@ -61,7 +61,7 @@ export const TradingChart = () => {
   useChartResize({ containerRef, chart });
 
   return (
-    <div className="flex min-h-[30rem] flex-col">
+    <div className="flex min-h-[35rem] flex-col">
       <section className="flex justify-end gap-5">
         {TIME_UNIT.map((option) => (
           <RadioButton
