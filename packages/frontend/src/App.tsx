a116c10852161fe91cc83d792b494ff884bedd32
baegyeong <beth0929@naver.com>
2024-12-05 06:20:25 +0900

    🚚 chore: react-error-boundary 설치 및 세팅

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { RouterProvider } from 'react-router-dom';
+import { Error } from './components/errors/error';
+import { Loader } from './components/ui/loader';
 import { router } from './routes';
 
 const queryClient = new QueryClient();
@@ -8,8 +12,12 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ErrorBoundary fallback={<Error />}>
+        <Suspense fallback={<Loader className="h-64 w-64 items-center" />}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Suspense>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 };
